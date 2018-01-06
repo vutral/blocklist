189b84a6d88b80554877ea0745d1df14f6b46e5e
vutral <vutral@dubnium.vutral.net>
2018-01-06 14:16:32 +0100

    multiple table support

--- cidr2ipset.pl
+++ cidr2ipset.pl
@@ -7,17 +7,21 @@ my $NAME='level1';
 if(defined($ARGV[0]) && $ARGV[0] ne '') {
   $NAME = $ARGV[0];
 }
+my $IPSET_NAME="blocklist";
+if(defined($ENV{'IPSET_NAME'}) && $ENV{'IPSET_NAME'} ne '') {
+  $IPSET_NAME = $ENV{'IPSET_NAME'};
+}
 
 my $cidrfile = "$NAME.cidr";
 
 open(CIDRF,$cidrfile);
 
-print "ipset destroy blocklist\n";
-print "ipset create blocklist hash:net maxelem 300000\n";
+print "ipset destroy ".$IPSET_NAME."\n";
+print "ipset create ".$IPSET_NAME." hash:net maxelem 400000\n";
 while(<CIDRF>) {
   my $line = $_;
   $line =~ s/\n//g;
   my $range = $line;
-  print "ipset add blocklist ".$range."\n";
+  print "ipset add ".$IPSET_NAME." ".$range."\n";
 }
 
