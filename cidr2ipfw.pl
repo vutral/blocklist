0594fc9f754080d6e6b412397ca1d12360b3e9bd
vutral <vutral@dubnium.vutral.net>
2018-01-06 13:56:56 +0100

    multiple table support

--- cidr2ipfw.pl
+++ cidr2ipfw.pl
@@ -3,15 +3,24 @@
 use strict;
 use warnings;
 
-my $cidrfile = "level1.cidr";
+my $NAME='level1';
+if(defined($ARGV[0]) && $ARGV[0] ne '') {
+  $NAME = $ARGV[0];
+}
+my $TABLE=5000;
+if(defined($ENV{'IPFW_TABLE'}) && $ENV{'IPFW_TABLE'} ne '') {
+  $TABLE=$ENV{'IPFW_TABLE'};
+}
+
+my $cidrfile = $NAME.".cidr";
 
 open(CIDRF,$cidrfile);
 
-print "ipfw table 5000 flush\n";
+print "ipfw table ".$TABLE." flush\n";
 while(<CIDRF>) {
   my $line = $_;
   $line =~ s/\n//g;
   my $range = $line;
-  print "ipfw -q table 5000 add ".$range."\n";
+  print "ipfw -q table ".$TABLE." add ".$range."\n";
 }
 
