15017eead812d1288a7ec8909e302b1819ff59b8
vutral <vutral@dubnium.vutral.net>
2018-01-06 08:09:15 +0100

    opnsense

--- cidr2opnsense.pl
+++ cidr2opnsense.pl
@@ -7,10 +7,11 @@ my $cidrfile = "level1.cidr";
 
 open(CIDRF,$cidrfile);
 
+my $n=0;
 while(<CIDRF>) {
   my $line = $_;
   $line =~ s/\n//g;
   my $range = $line;
-  print "".$range."\n";
+  print "".$range." ; N".$n++."\n";
 }
 
